f8745b43b1dbd0c8ad909a4741b92d3e0f746f4d
endsnipes <115029557+endsnipes@users.noreply.github.com>
2023-04-27 14:08:32 +0300

    add Search SpentCard

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import { ChangeEvent } from "react";
+import { StyledSearch } from "./styles";
+
+interface IProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Search = ({ value, onChange }: IProps) => {
+  return <StyledSearch placeholder="search ..." type="search" value={value} onChange={onChange} />;
+};
---
+++ src/components/SpentCard/SpentCard.tsx
@@ -0,0 +1,14 @@
+import { useCurrencyContext, useExpensesContext } from "../../context";
+import { StyledSpentCard } from "./styles";
+
+export const SpentCard = () => {
+  const { currentCurrency } = useCurrencyContext();
+  const { expenses } = useExpensesContext();
+  const spending = expenses.reduce((total, { price }) => total + +price, 0);
+
+  return (
+    <StyledSpentCard>
+      Spent so far: {spending} {currentCurrency.value}
+    </StyledSpentCard>
+  );
+};
