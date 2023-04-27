28044429d368b210452ebea2e9ba40bcb8f0699f
endsnipes <115029557+endsnipes@users.noreply.github.com>
2023-04-27 14:02:52 +0300

    add BudgetApp BudgetCard CustomSelect

--- src/components/Form/Form.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Form = () => {
-return ( 
-    <div>Form Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab magnam quia nam sapiente! Unde vel minus voluptatum laboriosam, quis impedit.</div>
-)
-}
